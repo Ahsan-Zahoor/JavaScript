c3349ba410856d2ac3f1252fe280cda4983ef2ea
ahsanzahoor633 <ahsan.zahoor@odysseyanalytics.net>
2023-06-20 00:43:10 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log(null == undefined); //true
+console.log(null === undefined); //false
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+let foo;
+console.log("is undefined ", foo === undefined); // true
+let fooo = { a: "a" };
+// console.log(foo.b); //undefined
+
+console.log(Infinity > -Infinity);
