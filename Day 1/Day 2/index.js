dcf10b385cd747ba7a4892a98dfc2e9e6cc2b27d
ahsanzahoor633 <ahsan.zahoor@odysseyanalytics.net>
2023-06-22 16:28:43 +0500

    Date timezones

--- Day 1/Day 2/index.js
+++ Day 1/Day 2/index.js
@@ -11,3 +11,21 @@ console.log(sentence.split("").reverse().join(""));
 // To cut white spaces from edges
 console.log("          some whitespaced string  ".trim());
 console.log("       this is me  ".trimStart());
+
+// Date
+var now = new Date();
+var nowOne = new Date(0);
+var one = new Date(2020, 1);
+var special = new Date(99, 0);
+console.log(special);
+
+var date1 = new Date();
+console.log(date1.toString()); //Returns: "Fri Apr 15 2016 07:48:48 GMT-0400 (Eastern Daylight Time)"
+console.log(date1.toTimeString()); //Returns: "07:48:48 GMT-0400 (Eastern Daylight Time)"
+console.log(date1.toDateString()); // Returns: "Thu Apr 14 2016"
+console.log(new Date().getFullYear());
+
+var checkoutDate = new Date();
+// Thu Jul 21 2016 10:05:13 GMT-0400 (EDT)
+checkoutDate.setDate(checkoutDate.getDate() + 12);
+console.log(checkoutDate); // Tue Aug 02 2016 10:05:13 GMT-0400 (EDT)
